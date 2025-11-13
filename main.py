555c47ae51b31e9869889318518d1cd8d00f3c85
DavidPopovich3261 <8534397@gmail.com>
2025-11-13 12:18:06 +0200

    main

--- main.py
+++ main.py
@@ -1,4 +1,41 @@
 from fastapi import FastAPI
 import uvicorn
-
+import Ciphers
+from pydantic import BaseModel
+import json
 app=FastAPI()
+
+
+class Caesar(BaseModel):
+    text:str
+    offset:int
+    mode:str
+
+class Fence(BaseModel):
+    text:str
+
+
+
+@app.post("/caesar")
+def caesar(item:Caesar):
+    if item.mode=="encrypt":
+        encrypt=Ciphers.caesar_encrypt(item.text,item.offset)
+        return json.dumps({"encrypted_text":encrypt})
+    elif item.mode=="decrypt":
+        decrypt=Ciphers.caesar_decrypt(item.text,item.offset)
+        return json.dumps({"decrypted_text":decrypt})
+
+
+
+@app.get("/fence/encrypt")
+def fence_encrypt(text):
+    encrypt=Ciphers.fence_encrypt(text)
+    return json.dumps({"encrypted_text":encrypt})
+
+
+
+@app.post("/fence/decrypt")
+def fence_decrypt(item:Fence):
+    decrypt=Ciphers.fence_decrypt(item.text)
+    return json.dumps({"decrypted":decrypt})
+
